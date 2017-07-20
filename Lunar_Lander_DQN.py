89cdbc7818839fd27dbe4212ec302f70980843e7
Aakash Kishore Chotrani <aakashchotrani@gmail.com>
2017-07-20 12:59:51 -0700

    Defined Training Method

--- Lunar_Lander_DQN.py
+++ Lunar_Lander_DQN.py
@@ -10,16 +10,21 @@ import numpy as np
 import tensorflow as tf
 
 
+REPLAY_SIZE = 100000
+BATCH_SIZE = 512
+
 class DQN:
     
     def __init__(self,state_dim,action_dim):
         self.stateDimension = state_dim
         self.actionDimension = action_dim
+        
         self.Neurons_Layer_1 = 128
         self.Neurons_Layer_2 = 256
         self.Neurons_Layer_3 = 512
-        
         self.Create_Neural_Network()
+        
+        self.replay_buffer = deque()
         
     
     def Create_Neural_Network(self):
@@ -41,6 +46,33 @@ class DQN:
         self.W4 = tf.get_variable("W4",[self.Neurons_Layer_3,self.actionDimension])
         self.b4 = tf.get_variable(tf.constant(0.01,shape = [self.actionDimension, ]))
         self.Q_value = tf.matmul(layer3,self.W4) + self.b4
+    
+    
+    def Training_Method(self):
+        self.action_input = tf.placeholder(tf.float32,[None,self.actionDimension])
+        self.y_input = tf.placeholder(tf.float32,[None])
+        Q_action = tf.reduce_sum(tf.multiply(self.Q_value,self.action_input),reduction_indices = 1)
+        
+        self.loss = tf.reduce_mean(tf.square(self.y_input - Q_action))
+        self.optimizer = tf.train.AdamOptimizer(0.0005).minimize(self.loss)
+        
+    
+    def perceive(self,state,action,reward,next_state,done):
+        one_hot_action_array = np.zeros(self.actionDimension)
+        one_hot_action_array[action] = 1
+        self.replay_buffer.append((state,one_hot_action_array,reward,next_state,done))
+        
+        #taking care of data overflow
+        if len(self.replay_buffer) > REPLAY_SIZE:
+            self.replay_buffer.popleft()
+        if len(self.replay_buffer) > BATCH_SIZE:
+            self.train_network()
+        
+    
+    def Train_Network(self):
+        
+        
+        
         
         
 
