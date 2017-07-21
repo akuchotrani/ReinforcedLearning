b868e2979efbb76c4f66debee0171279f2c4e774
Aakash Kishore Chotrani <aakashchotrani@gmail.com>
2017-07-21 15:07:16 -0700

    Changing model little

--- Lunar_Lander_DQN.py
+++ Lunar_Lander_DQN.py
@@ -10,6 +10,8 @@ import numpy as np
 import tensorflow as tf
 from collections import deque
 import random
+#import sys
+#from utility.utility import get_path
 
 REPLAY_SIZE = 100000
 BATCH_SIZE = 512
@@ -24,33 +26,40 @@ class DQN:
         self.stateDimension = state_dim
         self.actionDimension = action_dim
         
-        self.Neurons_Layer_1 = 128
-        self.Neurons_Layer_2 = 256
-        self.Neurons_Layer_3 = 512
-        self.Create_Neural_Network()
+        self.Neurons_Layer_1 = 20
+        self.Neurons_Layer_2 = 30
+        self.Neurons_Layer_3 = 40
         
         self.replay_buffer = deque()
         
         self.epsilon = INITIAL_EPSILON
+        
+        
+        self.Create_Neural_Network()
+        self.Training_Method()
+        self.session = tf.InteractiveSession()
+        self.session.run(tf.global_variables_initializer())
+        
     
     def Create_Neural_Network(self):
-        self.state_input = tf.placeholder(tf.float32,[None,self.state_dim], name = 'state_inputs')
+        print("create neural net called")
+        self.state_input = tf.placeholder(tf.float32,[None,self.stateDimension], name = 'state_inputs')
         
         self.W1 = tf.get_variable("W1",[self.stateDimension,self.Neurons_Layer_1])
-        self.b1 = tf.get_variable(tf.constant(0.01,shape = [self.Neurons_Layer_1, ]))
+        self.b1 = tf.Variable(tf.constant(0.01,shape = [self.Neurons_Layer_1, ]))
         layer1 = tf.nn.relu(tf.matmul(self.state_input,self.W1) + self.b1)
         
         self.W2 = tf.get_variable("W2",[self.Neurons_Layer_1,self.Neurons_Layer_2])
-        self.b2 = tf.get_variable(tf.constant(0.01,shape = [self.Neurons_Layer_2, ]))
+        self.b2 = tf.Variable(tf.constant(0.01,shape = [self.Neurons_Layer_2, ]))
         layer2 = tf.nn.relu(tf.matmul(layer1,self.W2) + self.b2)
         
         self.W3 = tf.get_variable("W3",[self.Neurons_Layer_2,self.Neurons_Layer_3])
-        self.b3 = tf.get_variable(tf.constant(0.01,shape = [self.Neurons_Layer_3, ]))
+        self.b3 = tf.Variable(tf.constant(0.01,shape = [self.Neurons_Layer_3, ]))
         layer3 = tf.nn.relu(tf.matmul(layer2,self.W3) + self.b3)
         
         
         self.W4 = tf.get_variable("W4",[self.Neurons_Layer_3,self.actionDimension])
-        self.b4 = tf.get_variable(tf.constant(0.01,shape = [self.actionDimension, ]))
+        self.b4 = tf.Variable(tf.constant(0.01,shape = [self.actionDimension, ]))
         self.Q_value = tf.matmul(layer3,self.W4) + self.b4
     
     
@@ -72,7 +81,7 @@ class DQN:
         if len(self.replay_buffer) > REPLAY_SIZE:
             self.replay_buffer.popleft()
         if len(self.replay_buffer) > BATCH_SIZE:
-            self.train_network()
+            self.Train_Network()
         
     
     def Train_Network(self):
@@ -98,42 +107,42 @@ class DQN:
                          self.action_input:action_batch,
                          self.state_input: state_batch}
             
-            self.session.run(self.optimizer,feed_dict)
+        self.session.run(self.optimizer,feed_dict)
             
             
             
-        def explore_action(self,state):
-            
-            Q_value = self.Q_value.eval(feed_dict = {self.state_input:[state]})[0]
-            
-            if self.epsilon > 0.1:
-                self.epsilon = self.epsilon - 0.001
-            else:
-                self.epsilon *= DECAY_RATE
-                
-                
-            if random.random() <= self.epsilon:
-                return random.randint(0,self.action_dim -1)
-            else:
-                return np.argmax(Q_value)
-            
+    def explore_action(self,state):
         
-        def action(self,state):
-            return np.argmax(self.Q_value.eval(feed_dict = {self.state_input:[state]})[0])
+        Q_value = self.Q_value.eval(feed_dict = {self.state_input:[state]})[0]
         
+        if self.epsilon > 0.1:
+            self.epsilon = self.epsilon - 0.001
+        else:
+            self.epsilon *= DECAY_RATE
+            
+            
+        if random.random() <= self.epsilon:
+            return random.randint(0,self.actionDimension -1)
+        else:
+            return np.argmax(Q_value)
         
-        def store_data(self,state,action,reward,next_state,done):
-            one_hot_action_array = np.zeros(self.actionDimension)
-            one_hot_action_array[action] = 1
-            self.replay_buffer.append((state,one_hot_action_array,reward,next_state,done))
-        
-            #taking care of data overflow
-            if len(self.replay_buffer) > REPLAY_SIZE:
-                self.replay_buffer.popleft()
-                
-        
-        def save_network(self,path):
-            self.saver.save(self.session,path + '/my_lunar_lander_model.ckpt')
+    
+    def action(self,state):
+        return np.argmax(self.Q_value.eval(feed_dict = {self.state_input:[state]})[0])
+    
+    
+    def store_data(self,state,action,reward,next_state,done):
+        one_hot_action_array = np.zeros(self.actionDimension)
+        one_hot_action_array[action] = 1
+        self.replay_buffer.append((state,one_hot_action_array,reward,next_state,done))
+    
+        #taking care of data overflow
+        if len(self.replay_buffer) > REPLAY_SIZE:
+            self.replay_buffer.popleft()
+            
+    
+    def save_network(self,path):
+        self.saver.save(self.session,path + '/my_lunar_lander_model.ckpt')
             
             
             
@@ -171,6 +180,47 @@ class Environment:
                 if done:
                     break
                 
+     def play_games_to_record_data(self,agent):
+         for i_episode in range(1000):
+             state = self.env.reset()
+             for step in range(500):
+                 action = agent.action(state)
+                 next_state,reward,done,info = self.env.step(action)
+                 agent.store_data(state,action,reward,next_state,done)
+                 state = next_state
+                 if done:
+                     break
+    
+     def  play_games_and_train(self,agent):
+         for i_episode in range(1000):
+             state = self.env.reset()
+             for step in range(500):
+                 action = agent.explore_action(state)
+                 next_state,reward,done,info = self.env.step(action)
+                 agent.perceive(state,action,reward,next_state,done)
+                 state = next_state
+                 if done:
+                     break
+                 if i_episode % 100 == 0:
+                     #agent.save_network(saveDir)
+                     total_reward = 0
+                     for i in range(10):
+                         state = self.env.reset()
+                         for j in range(500):
+                             self.env.render()
+                             action = agent.action(state)
+                             state,reward,done,info = self.env.step(action)
+                             total_reward += reward
+                             if done:
+                                 break
+                     average_reward = total_reward/500
+                     print("Episode: ",i_episode, " Average reward: ",average_reward)
+                    
+                     if average_reward >=200:
+                         break
+                 
+                 
+                
      def closeEnvironment(self):
         self.env.close()
                 
@@ -179,7 +229,10 @@ def main():
     env = Environment("LunarLander-v2")
     stateCnt  = env.env.observation_space.shape[0]
     actionCnt = env.env.action_space.n
-    env.run_random_action()
+    agent = DQN(stateCnt,actionCnt)
+    env.play_games_to_record_data(agent)
+   # saveDir = get_path('tmp/'+'lander%2d'%np.random.randint(99))
+    env.play_games_and_train(agent)
     env.closeEnvironment()
     
     
